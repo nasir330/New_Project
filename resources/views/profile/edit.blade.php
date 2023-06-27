845802c1e43d2669c01db72f52cdc2f34008cebd
DESKTOP-J25ULVV\Nasir <fmiexclusive@gmail.com>
2023-06-28 01:36:28 +0600

    profile edit page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -8,7 +8,6 @@
     </div>
 
     <div id="main_content">
-
         <!-- App Left-side-menu start -->
         @include('includes.menu.app_menu')
         <!-- App Left-side-menu end -->
@@ -130,5 +129,25 @@
             </div>
         </div>
     </div>
-
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <form action="#" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Upload Photo</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-center">
+                    <input type="hidden" name="contactId" value="{{Auth::user()->id}}">
+                    <input type="file" name="photo" class="form-control" required>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
     <x-footer />
